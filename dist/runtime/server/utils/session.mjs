e6532bc87a1d9b5daaf5a9437831819fce67a1b8
Sarah <sg@oxygen.dk>
2024-05-22 11:03:11 +0200

    removed require user session from get access token

--- dist/runtime/server/utils/session.mjs
+++ dist/runtime/server/utils/session.mjs
@@ -126,7 +126,6 @@ export async function getUserSessionId(event) {
   return (await _useSession(event)).id;
 }
 export async function getAccessToken(event) {
-  await requireUserSession(event);
   const sessionId = await getUserSessionId(event);
   const persistentSession = await useStorage("oidc").getItem(sessionId);
   const tokenKey = process.env.NUXT_OIDC_TOKEN_KEY;
--- src/runtime/server/utils/session.ts
+++ src/runtime/server/utils/session.ts
@@ -192,7 +192,7 @@ export async function getUserSessionId(event: H3Event) {
   return (await _useSession(event)).id as string
 }
 export async function getAccessToken(event: H3Event) {
-  await requireUserSession(event)
+  // await requireUserSession(event)
   const sessionId = await getUserSessionId(event)
   const persistentSession = await useStorage('oidc').getItem<PersistentSession>(sessionId as string) as PersistentSession | null
   const tokenKey = process.env.NUXT_OIDC_TOKEN_KEY as string
